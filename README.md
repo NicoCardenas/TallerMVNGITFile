808b67e67baa21081ef5c668a5f0373ff19f5d00
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-08-15 21:59:32 -0500

    documentacion

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # TallerMVNGITFile
+
+Este projecto consiste en una LinkedList de elementos tipo Double con los metodos para sacar media y desviacion estandar.
+
+## Classes
+
+![Modelo](https://www.planttext.com/plantuml/svg/UhzxlqDnIM9HIMbk3XTNSavYSR62JsPUTcgAJsPnYK8rbm8Gf6KM5PTOANYMfEQ2-K1Lo5nTNJ2Lc9AYeGbc5QNcvfUbGZcvgObvK2RGehoKnA8KbzIopCIIp7oGU2tCLf1nKFlA4dDAKwqKNF9Bat9ImI9vgHKm6u4Qa1oegsi74NP8pKi1-W4Wm000)
+
+
+## Versioning
+
+version 1.0
+
+## Authors
+
+* **Nicolas Diego Cardenas Guevara** ingeniero de sistemas de la Escuela Colombiana de Ingenieria Julio Garavito.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- mi-primera-app/src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
+++ mi-primera-app/src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
@@ -1,69 +1,131 @@
 package edu.escuelaing.arem.ASE.app;
 
+/**
+ * @author Nicolas Cardenas
+ * @version 1.0V
+ */
 public class LinkedList {
     private Item first;
     private Item last;
 
+    /**
+     * @author Nicolas Cardenas
+     * @version 1.0V
+     */
     private class Item {
         
         private Double value;
         private Item next;
         
+        /**
+         * 
+         * @param value valor con el cual se crea el item
+         */
         private Item (Double value) {
             this.value = value;
         }
         
+        /**
+         * 
+         * @return retorna la referencia del siguiente item
+         */
         private Item getNext() {
             return this.next;
         }
 
+        /**
+         * 
+         * @param item la referencia del siguiente item
+         */
         private void setNext(Item item) {
             this.next = item;
         }
 
+        /**
+         * @return Double valor del Item
+         */
         private Double getValue() {
             return value;
         }
 
+        /**
+         * 
+         * @param value asignacion del valor a un item
+         */
         private void setValue(Double value) {
             this.value = value;
         }
     }
     
+    /**
+     * Constructor de LinkedList
+     */
     public LinkedList(){
         this.first = this.last = new Item(0.0);
     }
 
-    public Item getFirst() {
-        return first;
+    /**
+     * retorna el valor en la primera posicion de la lista
+     * @return Double valor en la lista
+     */
+    public Double getFirst() {
+        return first.getValue();
     }
 
-    public void setFirst(Item first) {
-        this.first = first;
+    /**
+     * cambia el primer valor de la lista
+     * @param value valor a insertar
+     */
+    public void setFirst(Double value) {
+        this.first = new Item(value);
     }
 
-    public Item getLast() {
-        return last;
+    /**
+     * retorna el valor en la ultima posicion de la lista
+     * @return Double valor en la lista
+     */
+    public Double getLast() {
+        return last.getValue();
     }
 
-    public void setLast(Item last) {
-        this.last = last;
+    /**
+     * cambia el ultimo valor de la lista
+     * @param value valor a inserta
+     */
+    public void setLast(Double value) {
+        this.last = new Item(value);
     }
 
+    /**
+     * retorna el siguiete elemento de la lista
+     * @return Item elemento de la lista
+     */
     public Item next(){
         return this.first.getNext();
     }
 
-    public void add(Item item){
+    /**
+     * agrega un elemento a la lista en la ultima posicion
+     * @param value valor a insertar
+     */
+    public void add(Double value){
+        Item item = new Item(value);
         this.last.setNext(item);
         this.last = item;
     }
 
+    /**
+     * remueve el ultimo valor de la lista
+     */
     public void remove() {
         Item newFirst = this.first.getNext();
         this.first = newFirst;
     }
 
+    /**
+     * retorna el valos de la media de la lista
+     * @return valor de la media
+     */
     public Double mean() {
         int n = 0;
         Double sum = 0.0;
@@ -77,6 +139,10 @@ public class LinkedList {
         return res;
     }
 
+    /**
+     * retorna el valor de la desviacion estandar de la lista
+     * @return desviacion estandar
+     */
     public Double  standardDeviation() {
         Double Xavg = mean();
         int n = 0;
